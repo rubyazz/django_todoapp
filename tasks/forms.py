7e5de77ca8423b61220515b3ddbd8868c8520c27
rubyazz <ersultan.abduvalov@gmail.com>
2022-05-05 21:48:00 +0600

    commit with login and register

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,30 @@
 from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label="E-mail", widget=forms.EmailInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class':'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class':'form-control'}),
+        }
+
+
 
 from .models import *
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
 
 from .forms import *
 from .models import *
+
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'You have successfully registered')
+            return redirect('list')
+        else:
+            messages.error(request, "registration error")
+    else:
+        form = UserRegisterForm()
+    return render(request, "tasks/register.html", {"form":form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list')
+    else:
+        form = UserLoginForm()
+    return render(request, "tasks/login.html", {"form" : form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
 def index(request):
