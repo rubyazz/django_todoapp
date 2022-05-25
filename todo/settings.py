08b9e56ac03249630c76d7e82098ff0f938f87dc
rubyazz <ersultan.abduvalov@gmail.com>
2022-05-25 11:16:19 +0600

    debug false

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h7zlslv1=(zkrwjcr_ox5(w2(el4c%fjf)!qhc)#ao!f6jipsk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
